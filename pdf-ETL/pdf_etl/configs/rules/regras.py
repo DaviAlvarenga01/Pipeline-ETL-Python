1d615ae6064cd45fef185fc11ff5ae4cdd56626f
Davi Alvarenga <davialvarenga@hotmail.com.br>
2024-08-19 16:08:22 -0300

    add: terminando de criar as funções do objeto

--- pdf-ETL/pdf_etl/configs/rules/regras.py
+++ pdf-ETL/pdf_etl/configs/rules/regras.py
@@ -1,7 +1,20 @@
 jornada = {
+    "name": "Jornada",
+    "table_area": [""],
+    "columns": [""],
+    "flavour": "stream",
+    "page": ["1-end"],
+    "text_strip": "./n",
+    "password": None,
     
 }
 
 redrex = {
-    
+    "name": "Redrex",
+    "table_area": [""],
+    "columns": [""],
+    "flavour": "stream",
+    "page": ["1-end"],
+    "text_strip": "./n",
+    "password": None,
 }
