5f338a48c3017c8f8a6344878184e5e9caf3901b
Davi Alvarenga <davialvarenga@hotmail.com.br>
2024-08-20 01:10:08 -0300

    fix: código semi-funcional

--- pdf-ETL/src/pdf_viz.py
+++ pdf-ETL/src/pdf_viz.py
@@ -1,29 +1,27 @@
 import os
 import camelot
-import pandas
+import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
-file_name = 'corretora_jornada_de_dados (1)'
-path = os.path.abspath(f"pdf_etl/files/pdf/jornada/{file_name}.pdf")
+print(matplotlib.get_backend())
 
-tables = camelot.read_pdf(
-    path,
-    pages='1-end', #pega da primeira página até o final
-    flavor='stream', #tecnologia que vai usar para interpretar o pdf
-    table_areas=['65, 558, 500, 298'],
-    columns=["65, 107, 156, 212, 280, 336, 383, 450"],
-    strip_text=".\n",
-    password=""
-    
-)
+file_name = "Redrex - Fatura (2)"
+path = os.path.abspath(f"src/files/pdf/redrex/{file_name}.pdf")
 
+tables = camelot.read_pdf(path,
+                          flavor='stream',
+                          table_areas= ['70, 560,498,279'],
+                          columns= ['70, 105, 160, 230, 285, 340, 380, 446'],
+                          strip_text=".\n",
+                          pages="1-end",
+                          password="161"
+                          )
 print(tables[0].parsing_report)
-    
-#camelot.plot(tables[0], kind="contour")  #exibe como o software está enxergando o pdf
 
-#plt.show() 
+# camelot.plot(tables[0], kind="contour")
+
+# plt.show()
 
 print(tables[0].df)
-    
-print("Pause")
+print("Waiting for work")
